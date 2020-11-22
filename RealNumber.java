a64ffaae2b7647527957dfa9865bc65a47c7c869
Tina Nguyen <thetinalenguyen@gmail.com>
2020-11-22 02:34:46 -0500

    all the beginning stuff here

---
+++ RealNumber.java
@@ -0,0 +1,23 @@
+public class RealNumber {
+
+  private double value;
+
+  public RealNumber(double v){
+    value = v;
+  }
+
+  public double getValue() {
+    return value;
+  }
+
+  public String toString() {
+    return ""+getValue();
+  }
+
+  public boolean equals(RealNumber other)(
+//return true when values are within 0.001! of each other
+//if one value is 0, other must exactly be zero
+
+  )
+
+}
