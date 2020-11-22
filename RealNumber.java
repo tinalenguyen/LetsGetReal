1ba73da09dcb8742e6de4cc7fbead27bf1ee8084
Tina Nguyen <thetinalenguyen@gmail.com>
2020-11-22 03:23:37 -0500

    added equals method

--- RealNumber.java
+++ RealNumber.java
@@ -14,10 +14,18 @@ public class RealNumber {
     return ""+getValue();
   }
 
-  public boolean equals(RealNumber other)(
-//return true when values are within 0.001! of each other
+  public boolean equals(RealNumber other){
+//return true when values are within 0.001% of each other
 //if one value is 0, other must exactly be zero
+    if (other.getValue() == 0 && getValue() == 0){
+      return true;
+    }
 
-  )
+
+    if ( (Math.abs(other.getValue() - getValue())) / other.getValue() < 0.00001 ) {
+         return true;
+       }
+    else return false;
+  }
 
 }
